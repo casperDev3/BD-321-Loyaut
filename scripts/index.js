14e4bbad209fa6bace6a039bb16126a35a45fd12
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-09-13 17:41:51 +0300

    add first part

--- scripts/index.js
+++
@@ -1,6 +0,0 @@
-const BURGER_BTN = document.querySelector(".burger")
-BURGER_BTN.addEventListener("click", ()=>{
-    BURGER_BTN.classList.toggle('active')
-    const MOB_MENU = document.querySelector(".mobile_menu")
-    MOB_MENU.classList.toggle('active')
-})
